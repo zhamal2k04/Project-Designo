28540dd3aa91f01bd400e1674531d3ab9b8e8436
unknown <garippolosov@gmail.com>
2023-05-28 18:12:39 +0500

    added h1 main title and paragraph also one button into jsx

--- src/containers/HomePage/components/TopCard/TopCard.jsx
+++ src/containers/HomePage/components/TopCard/TopCard.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import "../TopCard/TopCard.css";
 
-const TopCard = () =>{
-    return(
-        <div className="topcard-container">
-            <div className="topcard-left-container">
-                
-            </div>
-        </div>
-    )
-}
+const TopCard = () => {
+  return (
+    <div className="topcard-container">
+      <div className="topcard-left-container">
+        <h1>
+          Award-winning custom <br /> designs and digital <br />
+          branding solutions
+        </h1>
+        <p>
+          With over 10 years in the industry, we are experienced in <br />{" "}
+          creating fully responsive websites, app design, and engaging <br />{" "}
+          brand experiences. Find out more about our services.
+        </p>
+        <button>Learn More</button>
+      </div>
+    </div>
+  );
+};
 
 export default TopCard;
