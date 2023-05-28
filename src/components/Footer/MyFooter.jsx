b26abeeabf29a3ccd51c01d27cdd3e3bda68426d
unknown <garippolosov@gmail.com>
2023-05-28 14:32:11 +0500

    into footer-frame added footer-logo and into it div oval for logo and h1

--- src/components/Footer/MyFooter.jsx
+++ src/components/Footer/MyFooter.jsx
@@ -5,7 +5,10 @@ const MyFooter = () =>{
     return (
         <div className="footer-container">
                 <div className="footer-frame">
-                    
+                    <div className="footer-logo">
+                        <div className="oval"></div>
+                        <h1>Designo</h1>
+                    </div>
                 </div>
         </div>
     )
