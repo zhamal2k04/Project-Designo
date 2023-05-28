aff4bd9b41761163e3868b474bc53780b09e4023
unknown <garippolosov@gmail.com>
2023-05-28 15:04:56 +0500

    imported css file into HomePage and added homepage-container

---
+++ src/containers/HomePage/container/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../container/HomePage.css";
+
+const HomePage = () =>{
+    return(
+        <div className="layout-container">
+
+        </div>
+    )
+}
+export default HomePage;
