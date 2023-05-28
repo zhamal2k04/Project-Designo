ee6e767d16ddfe8828a7f5ea3899f8788f424465
unknown <garippolosov@gmail.com>
2023-05-28 21:26:28 +0500

    added h1 title and paragraph into left container

--- src/containers/HomePage/components/LowerCard/LowerCard.jsx
+++ src/containers/HomePage/components/LowerCard/LowerCard.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "../LowerCard/LowerCard.css";
 
 const LowerCard = () => {
-  return <div className="lowerCard-container">
+  return (
+    <div className="lowerCard-container">
       <div className="lowerCard-left-container">
-
+        <h1>Let's talk about your project</h1>
+        <p>
+          Ready to take it to the next level? Contact us today and find out how
+          our expertise can help your business grow.
+        </p>
       </div>
-      <div className="lowerCard-right-container">
-        
-      </div>
-  </div>;
+      <div className="lowerCard-right-container"></div>
+    </div>
+  );
 };
 
 export default LowerCard;
