0a42274cb4147cfe5788963b82610720f2ec13ae
unknown <garippolosov@gmail.com>
2023-05-28 21:08:48 +0500

    imported LowerInfos and added into homepage-container

--- src/containers/HomePage/container/HomePage.jsx
+++ src/containers/HomePage/container/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../container/HomePage.css";
 import TopCard from "../components/TopCard/TopCard";
+import LowerInfos from "../components/LowerInfos/LowerInfos";
+import LinkedCards from "../components/LinkedCards/LinkedCards";
 const HomePage = () => {
   return (
     <div className="homepage-container">
       <TopCard />
       <LinkedCards />
+      <LowerInfos/>
     </div>
   );
 };
