7bad28fcee803fa6feadba76275dc2b5ba0c64f9
unknown <garippolosov@gmail.com>
2023-05-28 18:43:56 +0500

    added main title and paragraph into web-design div

--- src/containers/HomePage/components/LinkedCards/LinkedCards.jsx
+++ src/containers/HomePage/components/LinkedCards/LinkedCards.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "../LinkedCards/LinkedCards.css";
 
-const LinkedCards = () =>{
-    return (
-        <div className="linkedcards-container">
-            <div className="web-design">
-                
-            </div>
-        </div>
-    )
-}
+const LinkedCards = () => {
+  return (
+    <div className="linkedCards-container">
+      <div className="web-design">
+        <h1>Web Design</h1>
+        <p>
+          View Projects <span id="span"></span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default LinkedCards;
