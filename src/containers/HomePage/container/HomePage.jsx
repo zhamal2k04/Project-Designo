dd07a387ac438b885629a4ba2bba8070e04743b8
unknown <garippolosov@gmail.com>
2023-05-28 21:05:20 +0500

    re-commit

--- src/containers/HomePage/container/HomePage.jsx
+++ src/containers/HomePage/container/HomePage.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "../container/HomePage.css";
 import TopCard from "../components/TopCard/TopCard";
-import LinkedCards from "../components/LinkedCards/LinkedCards";
-const HomePage = () =>{
-    return(
-        <div className="homepage-container">
-            <TopCard/>
-            <LinkedCards/>
-        </div>
-    )
-}
+const HomePage = () => {
+  return (
+    <div className="homepage-container">
+      <TopCard />
+      <LinkedCards />
+    </div>
+  );
+};
 export default HomePage;
