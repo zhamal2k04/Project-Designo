782ce4ef453931d8cf651fb33ed4e6a6b07e8056
unknown <garippolosov@gmail.com>
2023-05-28 17:57:14 +0500

    imported HomePage and added it into Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import MyFooter from "./components/Footer/MyFooter";
 import MyNavbar from "./components/Navbar/MyNavbar";
 import MyLayout from "./components/Layout/MyLayout";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./containers/HomePage/container/HomePage";
 
 function App() {
   return (
     <div>
       <MyLayout>
-        
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
       </MyLayout>
     </div>
   );
