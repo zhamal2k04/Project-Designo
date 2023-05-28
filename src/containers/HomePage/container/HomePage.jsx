bc1049a9b954187b3d70ecced3626c8e5409c4ee
unknown <garippolosov@gmail.com>
2023-05-28 20:44:39 +0500

    imported LinkedCards and put into homepage-container

--- src/containers/HomePage/container/HomePage.jsx
+++ src/containers/HomePage/container/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../container/HomePage.css";
 import TopCard from "../components/TopCard/TopCard";
+import LinkedCards from "../components/LinkedCards/LinkedCards";
 const HomePage = () =>{
     return(
         <div className="homepage-container">
             <TopCard/>
+            <LinkedCards/>
         </div>
     )
 }
