89eb7b624a250bed95211fcd80bcc056e03b7df5
unknown <garippolosov@gmail.com>
2023-05-28 14:34:38 +0500

    into footer-frame added div footer-links that contains list ul

--- src/components/Footer/MyFooter.jsx
+++ src/components/Footer/MyFooter.jsx
@@ -9,6 +9,13 @@ const MyFooter = () =>{
                         <div className="oval"></div>
                         <h1>Designo</h1>
                     </div>
+                    <div className="footer-links">
+                        <ul>
+                            <li>Our Company</li>
+                            <li>Locations</li>
+                            <li>Contacts</li>
+                        </ul>
+                    </div>
                 </div>
         </div>
     )
