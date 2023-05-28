9de9c4884b7e10afe5ae646a808b6b07476c61d7
unknown <garippolosov@gmail.com>
2023-05-28 14:38:04 +0500

    imported MyFooter in App.jsx and added into div

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import MyFooter from './components/Footer/MyFooter';
 import MyNavbar from './components/Navbar/MyNavbar';
 function App() {
   return (
    <div>
       <MyNavbar />
+      <MyFooter />
    </div>
   );
 }
