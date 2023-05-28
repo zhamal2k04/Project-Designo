d55821b46775d5cb75652a7b3022be02e7a3d5c5
unknown <garippolosov@gmail.com>
2023-05-28 18:28:21 +0500

    imported TopCard jsx file and put into homepage-container

--- src/containers/HomePage/container/HomePage.jsx
+++ src/containers/HomePage/container/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../container/HomePage.css";
-
+import TopCard from "../components/TopCard/TopCard";
 const HomePage = () =>{
     return(
         <div className="homepage-container">
-
+            <TopCard/>
         </div>
     )
 }
