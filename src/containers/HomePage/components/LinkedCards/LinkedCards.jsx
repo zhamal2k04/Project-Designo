a63609f558bc0ae1b1f10b4f68e3d9699d519947
unknown <garippolosov@gmail.com>
2023-05-28 18:37:25 +0500

    exported LinkedCards imported css file into it

---
+++ src/containers/HomePage/components/LinkedCards/LinkedCards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../LinkedCards/LinkedCards.css";
+
+const LinkedCards = () =>{
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default LinkedCards;
