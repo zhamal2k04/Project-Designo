1400b936de4a6fb2b15570a807e9163edac7479d
unknown <garippolosov@gmail.com>
2023-05-28 14:47:27 +0500

    imported MyNavbar and MyFooter into MyLayout jsx

---
+++ src/components/Layout/MyLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import MyNavbar from "../Navbar/MyNavbar";
+import MyFooter from "../Footer/MyFooter";
+
+const MyLayout = () =>{
+    return(
+        <div></div>
+    )
+}
+
+export default MyLayout;
