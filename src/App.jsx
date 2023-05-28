ddb2429143b7ac84be3a03ccaeac22a40b2bfcfa
unknown <garippolosov@gmail.com>
2023-05-28 15:00:02 +0500

    added MyLayout into div

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
-import MyFooter from './components/Footer/MyFooter';
-import MyNavbar from './components/Navbar/MyNavbar';
-import MyLayout from './components/Layout/MyLayout';
-import { Routes, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import MyFooter from "./components/Footer/MyFooter";
+import MyNavbar from "./components/Navbar/MyNavbar";
+import MyLayout from "./components/Layout/MyLayout";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-   <div>
-      <MyNavbar />
-      <MyFooter />
-   </div>
+    <div>
+      <MyLayout>
+        
+      </MyLayout>
+    </div>
   );
 }
 
