649ceed6b1da472e900ee81c4f839bf52e1b6dd7
unknown <garippolosov@gmail.com>
2023-05-28 14:54:12 +0500

    imported MyLayout jsx and Routes, Route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import MyFooter from './components/Footer/MyFooter';
 import MyNavbar from './components/Navbar/MyNavbar';
+import MyLayout from './components/Layout/MyLayout';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
    <div>
