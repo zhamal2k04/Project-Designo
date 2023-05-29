2b57090707c7869fa31b03d28bd1428ec7f5d246
unknown <garippolosov@gmail.com>
2023-05-29 15:45:42 +0500

    imported AppLinkedCards into AppDesign and put into main div

--- src/containers/AppDesign/container/AppDesign.jsx
+++ src/containers/AppDesign/container/AppDesign.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "../container/AppDesign.css";
 import AppTopCard from "../components/AppTopCard/AppTopCard";
 import MainCards from "../components/MainCards/MainCards";
+import AppLinkedCards from "../components/AppLinkedCards/AppLinkedCards";
 const AppDesign = () => {
   return (
     <div className="appDesign-container">
         <AppTopCard/>
         <MainCards/>
+        <AppLinkedCards/>
     </div>
   );
 };
