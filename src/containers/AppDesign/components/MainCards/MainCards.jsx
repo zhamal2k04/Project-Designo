6044e3efd92348d15793275dbbe4767d24a48524
unknown <garippolosov@gmail.com>
2023-05-29 13:46:52 +0500

    imported css file and added div mainCard-handler

---
+++ src/containers/AppDesign/components/MainCards/MainCards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../MainCards/MainCards.css";
+
+const MainCards = () =>{
+    return(
+        <div className="mainCard-handler">
+
+        </div>
+    )
+}
+export default MainCards;
