695f10d1bdb09c36b012579fdcc67d0be2945b5b
unknown <garippolosov@gmail.com>
2023-05-29 14:32:35 +0500

    imported image and put into card-image div

--- src/containers/AppDesign/components/MainCards/MainCards.jsx
+++ src/containers/AppDesign/components/MainCards/MainCards.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../MainCards/MainCards.css";
-
+import card_img_1 from "../images/app-card-2.svg";
 const MainCards = () =>{
     return(
         <div className="mainCard-handler">
             <div className="mainCard-container">
                 <div className="app-card">
                     <div className="card-image">
-                        
+                        <img src={card_img_1} alt="image" />
                     </div>
                 </div>
             </div>
