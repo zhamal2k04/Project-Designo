895d07e41386e91f94e6b3003c2b7a8a70da26ff
unknown <garippolosov@gmail.com>
2023-05-29 15:27:51 +0500

    imported css file into AppLinkedCards.jsx

---
+++ src/containers/AppDesign/components/AppLinkedCards/AppLinkedCards.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../AppLinkedCards/AppLinkedCards.css";
+
+const AppLinkedCards = () =>{
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default AppLinkedCards;
