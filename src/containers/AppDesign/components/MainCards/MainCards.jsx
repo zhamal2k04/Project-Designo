5bdcd88f6fd40431d73605092a2f8d3b685aa8c9
unknown <garippolosov@gmail.com>
2023-05-29 14:42:58 +0500

    copy pasted all codes by app-card to app-card-p

--- src/containers/AppDesign/components/MainCards/MainCards.jsx
+++ src/containers/AppDesign/components/MainCards/MainCards.jsx
@@ -18,6 +18,20 @@ const MainCards = () =>{
                         </div>
                     </div>
                 </div>
+                <div className="app-card">
+                    <div className="card-image">
+                        <img src={card_img_1} alt="image" />
+                    </div>
+                    <div className="app-card-text">
+                        <div className="app-card-title">
+                            <h1>Express</h1>
+                        </div>
+                        <div className="app-card-p">
+                        <p>Solving the problem of poor indoor air quality by filtering the air</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
