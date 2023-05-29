2e298ad4ad8a9c6d575d68f088809f645f19edcd
unknown <garippolosov@gmail.com>
2023-05-29 15:30:56 +0500

    added appLinkedCard-img-0 section into left container into it h1 title and paragraph

--- src/containers/AppDesign/components/AppLinkedCards/AppLinkedCards.jsx
+++ src/containers/AppDesign/components/AppLinkedCards/AppLinkedCards.jsx
@@ -5,7 +5,10 @@ const AppLinkedCards = () =>{
     return(
         <div className="appLinkedCard-container">
             <div className="appLinkedCard-left-container">
-                
+                <section className="appLinkedCard-img-0">
+                    <h1>Web Design</h1>
+                    <p>View Projects <span style={{color: "chocolate"}}>→</span></p>
+                </section>
             </div>
         </div>
     )
