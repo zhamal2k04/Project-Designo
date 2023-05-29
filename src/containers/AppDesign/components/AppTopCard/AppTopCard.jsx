923b713543673c080f982e15272525298dedefdc
unknown <garippolosov@gmail.com>
2023-05-29 13:26:02 +0500

    imported css file and added div appTopCard-container

---
+++ src/containers/AppDesign/components/AppTopCard/AppTopCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../AppTopCard/AppTopCard.css";
+
+const AppTopCard = () =>{
+    return(
+        <div className="appDesign-container">
+
+        </div>
+    )
+}
+
+export default AppTopCard;
