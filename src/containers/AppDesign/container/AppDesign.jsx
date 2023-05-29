4a1d4a8538b097fe5dda192a2435cbc25b3f4a32
unknown <garippolosov@gmail.com>
2023-05-29 15:09:10 +0500

    imported MainCards and put into appDesign-container div

--- src/containers/AppDesign/container/AppDesign.jsx
+++ src/containers/AppDesign/container/AppDesign.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../container/AppDesign.css";
 import AppTopCard from "../components/AppTopCard/AppTopCard";
+import MainCards from "../components/MainCards/MainCards";
 const AppDesign = () => {
   return (
     <div className="appDesign-container">
         <AppTopCard/>
+        <MainCards/>
     </div>
   );
 };
