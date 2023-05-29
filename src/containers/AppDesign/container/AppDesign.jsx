1863ea7c02170da64ad1df08c33aa5c9044d939c
unknown <garippolosov@gmail.com>
2023-05-29 13:16:25 +0500

    added h1 title and paragraph into appDesign-main-title

--- src/containers/AppDesign/container/AppDesign.jsx
+++ src/containers/AppDesign/container/AppDesign.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import "../container/AppDesign.css";
 
-const AppDesign = () =>{
-    return (
-        <div className="appDesign-container">
-            <div className="appDesign-main-title">
-
-            </div>
-        </div>
-    )
-}
+const AppDesign = () => {
+  return (
+    <div className="appDesign-container">
+      <div className="appDesign-main-title">
+        <h1>App Design</h1>
+        <p>
+          Our mobile designs bring intuitive digital solutions <br /> to your
+          customers right at their fingertips.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default AppDesign;
