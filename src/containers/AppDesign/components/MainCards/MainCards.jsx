b57aae38fc4e1e59291e4ec2ad89af1896449047
unknown <garippolosov@gmail.com>
2023-05-29 15:02:01 +0500

    copy pasted mainCard-container that contains 2 cards init

--- src/containers/AppDesign/components/MainCards/MainCards.jsx
+++ src/containers/AppDesign/components/MainCards/MainCards.jsx
@@ -46,7 +46,34 @@ const MainCards = () =>{
                         </div>
                     </div>
                 </div>
-
+            </div>
+            <div className="mainCard-container">
+                <div className="app-card">
+                    <div className="card-image">
+                        <img src={card_img_1} alt="image" />
+                    </div>
+                    <div className="app-card-text">
+                        <div className="app-card-title">
+                            <h1>Express</h1>
+                        </div>
+                        <div className="app-card-p">
+                        <p>Solving the problem of poor indoor air quality by filtering the air</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="app-card">
+                    <div className="card-image" id="cardNum2">
+                        <img src={card_img_2} alt="image" />
+                    </div>
+                    <div className="app-card-text">
+                        <div className="app-card-title">
+                            <h1>Eyecam</h1>
+                        </div>
+                        <div className="app-card-p">
+                        <p>Product that lets you edit your favorite photos and videos at any time</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
