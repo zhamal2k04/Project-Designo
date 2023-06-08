62718543b67a583a9de8e7506087fca5fd3a4c67
unknown <garippolosov@gmail.com>
2023-06-08 22:25:00 +0500

    added useStates and useEffect for LoginPage to validate signup

--- src/containers/LoginPage/components/LoginCard/LoginCard.jsx
+++ src/containers/LoginPage/components/LoginCard/LoginCard.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "../LoginCard/LoginCard.css"
 import google_icon from "../../images/google-icon.png"
+import { useState } from 'react'
 const LoginCard = () => {
+    // const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [emailDirty, setEmailDirty] = useState(false)
+    const [passwordDirty, setPasswordDirty] = useState(false)
+    const [emailError, setEmailError] = useState("Email Cannot be Empty")
+    const [passwordError, setPasswordError] = useState("Password Cannot be Empty")
+    const [formValid, setFormValid] = useState(false)
+
+    useEffect(() =>{
+        if(emailError || passwordError){
+            setFormValid(false)
+        }else{
+            setFormValid(true)
+        }
+    }, [emailError, passwordError])
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+        const re = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/
+
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Incorrect Email!')
+        } else {
+            setEmailError("")
+        }
+    }
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value)
+        if (e.target.value.length < 3 || e.target.value.length > 8){
+            setPasswordError('Password length should be more than 3 and 8!')
+            if(!e.target.value){
+                setPasswordError("Password Cannot be Empty")
+            }
+        }else{
+            setPasswordError("")
+        }
+    }
+  
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'password':
+                setPasswordDirty(true)
+                break
+        }
+    }
+
+    
     return (
         <div className='loginCard-container'>
-            <div className="loginCard-left">
-
-            </div>
+            <div className="loginCard-left"></div>
             <div className="loginCard-right">
                 <div className="title-content">
                     <h1>Join us Today 👋</h1>
@@ -21,12 +72,14 @@ const LoginCard = () => {
                         <p>First & Last Name</p>
                         <input type="text" placeholder='i.e Devon Larratt' />
                         <p>Email Address</p>
-                        <input type="email" placeholder='i.e devon@mail.com' />
+                        {(emailDirty && emailError) && <div style={{ color: 'red' }}>{emailError}</div>}
+                        <input onChange={e => emailHandler(e)} value={email} onBlur={e => blurHandler(e)} type="email" name='email' placeholder='i.e devon@mail.com' />
                         <p>Password</p>
-                        <input type="password" placeholder='*********' />
+                        {(passwordDirty && passwordError) && <div style={{ color: 'red' }}>{passwordError}</div>}
+                        <input onChange={e => passwordHandler(e)} value={password} onBlur={e => blurHandler(e)} type="password" name='password' placeholder='*********' />
+                    <button disabled={!formValid} type='submit' id='sign-btn'>Sign Up</button>
                     </form>
                     <span>Forgot the Password?</span>
-                    <button id='sign-btn'>Sign Up</button>
                 </div>
             </div>
         </div>
